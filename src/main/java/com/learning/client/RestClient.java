b578bddbf8593a07599394a3cec89eb3b3320806
Said Dinar Fadilah <saiddinarfadilah@gmail.com>
2024-06-09 00:06:20 +0700

    feat:rest client resttemplate

---
+++ src/main/java/com/learning/client/RestClient.java
@@ -0,0 +1,51 @@
+package com.learning.client;
+
+import com.learning.exception.APIException;
+import com.learning.exception.GeneralException;
+import com.learning.util.StringUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+
+@Component
+@Slf4j
+public class RestClient {
+
+
+    private final RestTemplate restTemplate;
+    private final StringUtil stringUtil;
+
+    public RestClient(RestTemplate restTemplate, StringUtil stringUtil) {
+        this.restTemplate = restTemplate;
+        this.stringUtil = stringUtil;
+    }
+
+    public Object callAPI(String destination, String url, String method, Object request) throws APIException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        HttpEntity<Object> entity = new HttpEntity<>(request, headers);
+
+        try {
+            log.info("<<<<<  REST CLIENT  >>>>>");
+            log.info("{} request  : {} {} headers : {}, body : {}" , destination, HttpMethod.valueOf(method), url, entity.getHeaders(), stringUtil.toString(entity.getBody()));
+            ResponseEntity<Object> exchange = restTemplate.exchange(
+                    url,
+                    HttpMethod.valueOf(method),
+                    entity,
+                    Object.class);
+            log.info("{} response : status code : {}, body : {}, process in 0 ms", destination, exchange.getStatusCode(), stringUtil.toString(exchange.getBody()));
+            log.info("<<<<<<<<<<<<>>>>>>>>>>>>>");
+            return exchange.getBody();
+        } catch (RestClientException | GeneralException e) {
+            log.error(e.getMessage());
+            throw new APIException("Error callAPI() : " + e.getMessage());
+        }
+    }
+}
--- src/main/java/com/learning/service/ResourceService.java
+++ src/main/java/com/learning/service/ResourceService.java
@@ -1,11 +1,14 @@
 package com.learning.service;
 
+import com.learning.client.RestClient;
 import com.learning.entity.Resource;
+import com.learning.exception.APIException;
 import com.learning.exception.DatabaseException;
 import com.learning.exception.GeneralException;
 import com.learning.repository.impl.ResourceRepositoryImpl;
 import com.learning.util.StringUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedHashMap;
@@ -15,31 +18,51 @@ import java.util.Map;
 @Slf4j
 public class ResourceService {
 
+    @Value("${url.api}")
+    private String apiUrl;
+
     private final ResourceRepositoryImpl resourceRepository;
+    private final RestClient restClient;
     private final StringUtil stringUtil;
 
-    public ResourceService(ResourceRepositoryImpl resourceRepository, StringUtil stringUtil) {
+    public ResourceService(ResourceRepositoryImpl resourceRepository, RestClient restClient, StringUtil stringUtil) {
         this.resourceRepository = resourceRepository;
+        this.restClient = restClient;
         this.stringUtil = stringUtil;
     }
 
-    public String createSource() throws DatabaseException {
+    public String createSource() {
         Resource resource = new Resource();
         resource.setSourceName("database");
-        resourceRepository.save(resource);
-        return "source save successfuly";
+
+        try {
+            resourceRepository.save(resource);
+            return "source save successfully";
+        } catch (DatabaseException e) {
+            log.error(e.getMessage());
+        }
+
+        return null;
     }
 
-    public Object fetchResource(String resourceName) throws DatabaseException, GeneralException {
-        Map<String, Object> resource = new LinkedHashMap<>();
+    public Object fetchResource(String resourceName) {
         if (resourceName.equals("api")) {
-            resource.put("resource", "api");
-            log.info("Outgoing Response : {}", resource);
-            return resource;
+            try {
+                Object resource = restClient.callAPI("something", apiUrl, "GET", null);
+                log.info("Outgoing Response : {}", stringUtil.toString(resource));
+                return resource;
+            } catch (APIException | GeneralException e) {
+                log.error(e.getMessage());
+            }
         } else {
-            Resource result = resourceRepository.find();
-            log.info("Outgoing Response : {}", stringUtil.toString(result));
-            return result;
+            try {
+                Resource resource = resourceRepository.find();
+                log.info("Outgoing Response : {}", stringUtil.toString(resource));
+                return resource;
+            } catch (DatabaseException | GeneralException e) {
+                log.error(e.getMessage());
+            }
         }
+        return null;
     }
 }
